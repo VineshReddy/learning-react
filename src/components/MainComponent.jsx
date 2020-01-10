985aaaeb542f97fc79160cd0ac98564003f53c8f
Vinesh Reddy <vineshreddy1997@gmail.com>
2020-01-10 19:13:47 +0530

    Combine Reducers

--- src/components/MainComponent.jsx
+++ src/components/MainComponent.jsx
@@ -22,10 +22,6 @@ const mapStateToProps = (state) => {
 
 
 class Main extends Component {
-  constructor(props) {
-    super(props);
-  }
-
   render() {
     const HomePage = () => {
       return (
